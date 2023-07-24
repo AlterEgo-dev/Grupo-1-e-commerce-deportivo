8a10ed5e6d591c4c8848f7149991dc92c8808fc8
bruno7even <brunob2013@hotmail.com.ar>
2023-07-23 23:07:25 -0300

    el boton no se cargaba cuando recargabamos la pagina

--- public/js/login-slider.js
+++ public/js/login-slider.js
@@ -59,29 +59,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
       /* FUNCION MANTENER EL COLOR SOBRE EL CLICK */
 
-    const loginBtn = document.getElementById('login-btn');
-    const registerBtn = document.getElementById('register-btn');
-    const loginForm = document.getElementById('login-form');
-    const registerForm = document.getElementById('register-form');
+      const loginBtn = document.getElementById('login-btn');
+      const registerBtn = document.getElementById('register-btn');
+      const loginForm = document.getElementById('login-form');
+      const registerForm = document.getElementById('register-form');
+  
+      // Function to handle the button clicks and toggle the selected class
+      function handleButtonClick(selectedButton) {
+          loginBtn.classList.remove('selected');
+          registerBtn.classList.remove('selected');
+          selectedButton.classList.add('selected');
+      }
+  
+      // Event listeners to switch between forms
+      loginBtn.addEventListener('click', () => {
+          loginForm.style.display = 'block';
+          registerForm.style.display = 'none';
+          handleButtonClick(loginBtn);
+      });
+  
+      registerBtn.addEventListener('click', () => {
+          loginForm.style.display = 'none';
+          registerForm.style.display = 'block';
+          handleButtonClick(registerBtn);
+      });
 
-    
-    function handleButtonClick(selectedButton) {
-        loginBtn.classList.remove('selected');
-        registerBtn.classList.remove('selected');
-        selectedButton.classList.add('selected');
-    }
-
-    
-    loginBtn.addEventListener('click', () => {
-        loginForm.style.display = 'block';
-        registerForm.style.display = 'none';
-        handleButtonClick(loginBtn);
-    });
-
-    registerBtn.addEventListener('click', () => {
-        loginForm.style.display = 'none';
-        registerForm.style.display = 'block';
-        handleButtonClick(registerBtn);
-    });
-    
 /* ----------------------------------------------- */
