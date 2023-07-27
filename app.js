f0b488d46c2132dff1710910153ef4e1e9d25382
bruno7even <brunob2013@hotmail.com.ar>
2023-07-27 19:46:41 -0300

    añadiendo dependencias y librerias

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const puerto = process.env.puerto || 8000;
+
+app.use(express.json());
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.get('/login-register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login-register.html'))
+})
+
+app.get('/detalle-producto', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/detalle-producto.html'))
+})
+
+app.listen(puerto, () => {
+    console.log(`Aplicación corriendo en puerto ${puerto}`);
+});
