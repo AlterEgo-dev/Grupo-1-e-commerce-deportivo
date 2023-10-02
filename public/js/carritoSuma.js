dd0637c112647396a26cf02c1d9b446b122d2825
bruno7even <brunob2013@hotmail.com.ar>
2023-10-02 18:44:13 -0300

    alert cambios sin guardar en perfil

--- public/js/carritoSuma.js
+++ public/js/carritoSuma.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const subtotal = document.getElementById("subtotal");
     const resultPrice = parseFloat(document.querySelector("#subtotal").textContent); // parseamos como numero al subtotal
   
-    selectCantidad.addEventListener("change", function() { // que escuche un cambio
-      const cantidad = parseInt(selectCantidad.value, 10); // parseamos
-      subtotal.textContent = resultPrice * cantidad;
+    selectCantidad.addEventListener("change", function() { // funcion para que escuche un cambio
+      const cantidad = parseInt(selectCantidad.value, 10); // parseamos el select y definimos un valor maximo de 10
+      subtotal.textContent = resultPrice * cantidad; 
     });
   });
   
