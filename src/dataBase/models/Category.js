be211b9734e10e52cb9ef926153ff4647f2332d5
juliidiaz <juliana.diaz@live.com>
2023-10-08 21:02:46 -0300

    modelo Category

---
+++ src/dataBase/models/Category.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, dataTypes) => {
+    let Category = sequelize.define(
+        'Category',
+        {id: {
+            type: dataTypes.INTEGER(11),
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+            },
+            Category: {
+                type: dataTypes.VARCHAR(100),
+            }
+        },
+        {
+            tableName: 'categories',
+            timestamps: false,
+        });
+       // Category.associate = function (models) {
+         //   Category.hasMany(models.Product, {
+           //     as: "products",
+             //   foreignKey: "Categories_id"
+       //     })
+        //}
+        return Category
+    };
