f9dc687bc4e6a50c89a82b7379faf8cfc23cdbef
tomassantacreu <tomassantacreu12@gmail.com>
2023-11-14 21:53:09 -0300

    empezando api products

--- src/controllers/api/apiProductsController.js
+++ src/controllers/api/apiProductsController.js
@@ -1,1 +1,12 @@
 // ACÁ HAY QUE TRAER DE LA BD DE TODOS LOS PRODUCTOS Y HACER UN RES.JSON PARA VER SOLO EL JSON DEL MISMO
+const db = require('../../dataBase/models')
+
+module.exports = {
+    list: (req, res) => {
+        db.Product
+        .findAll()
+        .then(products =>{
+            return res.json(products)
+        })
+    }
+}
