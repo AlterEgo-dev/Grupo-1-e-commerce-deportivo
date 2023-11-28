c246bcfd12628bba2ee78d5667d47b24a5fa9971
alexislan <alexislanchiotti13@gmail.com>
2023-11-27 22:24:02 -0300

    tenemos carrito funcional :)

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,7 @@ const path = require('path');
 // const dataBase = require('../dataBase/productList.json')
 const db =require ("../dataBase/models")
 
+
 const mainController = {
 
     home: (req, res) => {
@@ -12,10 +13,48 @@ const mainController = {
     },
     
     carrito: (req, res) => {
-        const { id } = req.params;
-        const { results } = dataBase;
-        const result = results.find((prod) => prod.id === id);
-        res.render('carrito.ejs', { result });
+        let result = [];
+        let promises = [];
+        let subtotal = 0;
+        req.session.miArray = req.session.miArray || [];
+        if(req.session.miArray.length > 0){
+            for (let i = 0; i < req.session.miArray.length; i++) {
+                let promise = db.Product.findByPk(req.session.miArray[i])
+                    .then((product) => {
+                        result[i] = product;
+                        subtotal += parseInt(product.Price);
+                    });
+                promises.push(promise);
+            }
+        
+            Promise.all(promises)
+                .then(() => {
+                    res.render('carrito.ejs', { result, subtotal });
+                    console.log(result);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send('Error interno del servidor');
+                });
+        }else{
+            result = null;
+            res.render('carrito.ejs', { result, subtotal });
+        }
+    },
+    carritoId: (req,res,next) => {
+        const ids = req.params.id;
+        req.session.miArray = req.session.miArray || [];
+        const verifId = req.session.miArray.includes(ids);
+        if(verifId == false){
+            req.session.miArray.push(ids);
+        }
+        res.redirect("/carrito")
+    },
+    borrarCarrito: (req,res) => {
+        const id = req.params.id;
+        const nuevoArray = req.session.miArray.filter(idA => idA != id)
+        req.session.miArray = nuevoArray;
+        res.redirect("/carrito")
     }
 }
 
--- src/routes/main.js
+++ src/routes/main.js
@@ -7,6 +7,8 @@ router.get('/', mainController.home);
 
 router.get('/carrito', authSession, mainController.carrito); /* de momento el carrito solo estara disponible para usuarios logueados, más la vista de detalle para todo publico */
 
-router.get('/carrito/:id', authSession, mainController.carrito);
+router.get('/borrar/:id', authSession, mainController.borrarCarrito)
+
+router.get('/carrito/:id', authSession, mainController.carritoId);
 
 module.exports = router;
